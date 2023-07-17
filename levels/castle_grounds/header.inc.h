6fa89d431cc6599b60c5c5399596d09d64c69766
TheButterMage <charlie.r.brady@gmail.com>
2023-07-17 17:26:41 +0100

    Castle Grounds replaced by fast64 template

---
+++ levels/castle_grounds/header.inc.h
@@ -0,0 +1,10 @@
+extern const GeoLayout castle_grounds_area_1_geo[];
+extern const GeoLayout castle_grounds_area_1[];
+extern const Collision castle_grounds_area_1_collision[];
+extern const MacroObject castle_grounds_area_1_macro_objs[];
+extern Lights1 castle_grounds_dl_sm64_material_001_lights;
+extern Vtx castle_grounds_dl_Cube_mesh_layer_1_vtx_0[24];
+extern Gfx castle_grounds_dl_Cube_mesh_layer_1_tri_0[];
+extern Gfx mat_castle_grounds_dl_sm64_material_001[];
+extern Gfx castle_grounds_dl_Cube_mesh_layer_1[];
+extern Gfx castle_grounds_dl_material_revert_render_settings[];
